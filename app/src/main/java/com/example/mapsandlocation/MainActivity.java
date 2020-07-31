53b8557885ceb5c526f69a959cd560eef63cb52b
Rahul kumar <rahulparida933@gamil.com>
2020-07-31 14:04:32 +0530

    login activity added

---
+++ app/src/main/java/com/example/mapsandlocation/MainActivity.java
@@ -0,0 +1,31 @@
+package com.example.mapsandlocation;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button browseMap;
+    Button loginBtn;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        browseMap = findViewById(R.id.browseMap);
+        loginBtn = findViewById(R.id.loginBtn);
+
+        browseMap.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(MainActivity.this, MapsActivity.class);
+                startActivity(i);
+            }
+        });
+    }
+}
